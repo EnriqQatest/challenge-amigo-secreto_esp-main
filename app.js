7fa4f2b45f72a36fe71892525f7a179757574b72
Enrique De Los Santos <enriqtester@gmail.com>
2025-07-30 17:00:15 -0400

    Se Agrega Variable amigo, funcion y condicional

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables del proyecto
+   let amigo = [];
+
+   //Funcion
+   function agregarAmigo(){
+      let inputAmigo = document.getElementById("amigo");
+      let nombreAmigo = inputAmigo.value;
+
+      //Condicional
+      If (!amigo) {
+         alert ("Debes ingresar un nombre");
+         return
+      }
+ }
