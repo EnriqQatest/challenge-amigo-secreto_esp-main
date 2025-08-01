152bcc98333b0846a03aa2f0b8d22354607f24da
Enrique De Los Santos <enriqtester@gmail.com>
2025-07-31 21:38:16 -0400

    Se realiza Test con console.log

--- app.js
+++ app.js
@@ -9,10 +9,10 @@
       let nombreAmigo = inputAmigo.value;
 
       //Condicional
-      if (!nombreAmigo) {
+      if (!nombreAmigo) {/*El if va en minuscula--- con el ! se esta negando la variable*/
          alert ("Debes ingresar un nombre");
          return
       }
-      amigo.push(nombreAmigo);
+      amigo.push(nombreAmigo);/*Seguido se llama a la lista de amigos */ 
       console.log(amigo);     
  }
